8820ec18298ea385a42e06e1b99e42baedd50535
Lemonish <patrikmihelson@gmail.com>
2025-06-24 19:18:39 +0300

    removed extra y from global yank

--- nvim/lua/lemonish/remap.lua
+++ nvim/lua/lemonish/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>dv", vim.cmd.Ex)
 vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action, { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+yy', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+yy', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+y', { noremap = true })
 vim.keymap.set({ "n", "v" }, "=", function()
     vim.lsp.buf.format()
 end, { noremap = true, silent = true })
