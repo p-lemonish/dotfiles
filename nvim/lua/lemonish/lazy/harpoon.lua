39ecea04947198b9140a74278cd20573cdc7345c
Lemonish <patrikmihelson@gmail.com>
2025-06-28 23:54:50 +0300

    updated keybinds

--- nvim/lua/lemonish/lazy/harpoon.lua
+++ nvim/lua/lemonish/lazy/harpoon.lua
@@ -10,15 +10,15 @@ return {
 
             vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<C-y>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end)
+            vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-u>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-i>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-o>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><C-p>", function() harpoon:list():replace_at(4) end)
     end
 }
