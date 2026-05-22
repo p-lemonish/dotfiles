81eaa65a97f511aff121ef1289f4c531c614e631
Lemonish <lemonish22@pm.me>
2026-05-22 15:07:16 +0300

    lsp.lua: relax python formatter and linting

--- nvim/lua/lemonish/lazy/lsp.lua
+++ nvim/lua/lemonish/lazy/lsp.lua
@@ -4,34 +4,23 @@ return {
     "stevearc/conform.nvim",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+
+    "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "saadparwaiz1/cmp_luasnip",
+
     "L3MON4D3/LuaSnip",
     "j-hui/fidget.nvim",
-    {
-      "hrsh7th/nvim-cmp",
-      config = function()
-        require 'cmp'.setup {
-          snippet = {
-            expand = function(args)
-              require 'luasnip'.lsp_expand(args.body)
-            end
-          },
-          sources = {
-            { name = 'luasnip' },
-          },
-        }
-      end
-    },
   },
 
   config = function()
-    local cmp = require('cmp')
+    local cmp = require("cmp")
     local cmp_lsp = require("cmp_nvim_lsp")
     local luasnip = require("luasnip")
+
     local capabilities = vim.tbl_deep_extend(
       "force",
       {},
@@ -41,14 +30,7 @@ return {
 
     require("fidget").setup({})
 
-    -- Auto format on save
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-      end,
-    })
-
+    -- Completion
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -57,14 +39,12 @@ return {
       },
 
       mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping(function(fallback)
+        ["<CR>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             if luasnip.expandable() then
               luasnip.expand()
             else
-              cmp.confirm({
-                select = true,
-              })
+              cmp.confirm({ select = true })
             end
           else
             fallback()
@@ -93,15 +73,21 @@ return {
       }),
 
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
       }, {
-        { name = 'buffer' },
-      })
+        { name = "buffer" },
+        { name = "path" },
+      }),
     })
 
+    -- Diagnostics UI
     vim.diagnostic.config({
-      -- update_in_insert = true,
+      virtual_text = true,
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
       float = {
         focusable = false,
         style = "minimal",
@@ -112,42 +98,128 @@ return {
       },
     })
 
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        --"ruby_lsp", -- must do apt install gem and apt install ruby-dev, then do gem install ruby-lsp.
-        "lua_ls",
-        "basedpyright",
-        "ruff",
-        "clangd",
-        "rust_analyzer",
-        "terraformls",
-        "ts_ls",
-        "gopls",
+    -- Formatting
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+        markdown = { "trim_whitespace" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        terraform = { "terraform_fmt" },
+        ruby = { "rubocop" },
+        go = { "gofmt" },
       },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities
-          }
-        end,
-      },
-
-      require("conform").setup({
-        formatters_by_ft = {
-          lua             = { "stylua" },
-          python          = { "ruff_format" },
-          markdown        = { "trim_whitespace" },
-          javascript      = { "prettier" },
-          javascriptreact = { "prettier" },
-          typescriptreact = { "prettier" },
-          typescript      = { "prettier" },
-          html            = { "prettier" },
-          terraform       = { "terraform_fmt" },
-          ruby            = { "rubocop" },
-          go              = { "gofmt" },
-        },
-      })
     })
-  end
+
+    -- Auto format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        if vim.bo[args.buf].filetype == "python" then
+          local lines = table.concat(vim.api.nvim_buf_get_lines(args.buf, 0, 50, false), "\n")
+          if lines:match("from%s+pwn%s+import%s+%*") or lines:match("import%s+pwn") then
+            return
+          end
+        end
+        require("conform").format({
+          bufnr = args.buf,
+          lsp_fallback = true,
+        })
+      end,
+    })
+
+    local servers = {
+      "lua_ls",
+      "basedpyright",
+      "ruff",
+      "clangd",
+      "rust_analyzer",
+      "terraformls",
+      "ts_ls",
+      "gopls",
+    }
+
+    for _, server in ipairs(servers) do
+      vim.lsp.config(server, {
+        capabilities = capabilities,
+      })
+    end
+
+    -- Lua / Neovim config
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
+
+    vim.lsp.config("basedpyright", {
+      settings = {
+        basedpyright = {
+          analysis = {
+            typeCheckingMode = "off",
+            diagnosticMode = "openFilesOnly",
+            diagnosticSeverityOverrides = {
+              reportAny = "none",
+              reportExplicitAny = "none",
+              reportUnknownMemberType = "none",
+              reportUnknownVariableType = "none",
+              reportUnknownLambdaType = "none",
+              reportMissingTypeStubs = "none",
+              reportUnknownParameterType = "none",
+              reportUnknownArgumentType = "none",
+              reportUnusedImport = "none",
+              reportUnusedVariable = "none",
+              reportUnusedFunction = "none",
+              reportWildcardImportFromLibrary = "none",
+              reportUndefinedVariable = "none",
+              reportPossiblyUnboundVariable = "warning",
+              reportUnboundVariable = "warning",
+            },
+          },
+        },
+      },
+    })
+
+    -- Add more ignores here if needed
+    vim.lsp.config("ruff", {
+      init_options = {
+        settings = {
+          lint = {
+            select = { "E", "F", "I", "UP", "B" },
+            ignore = {
+              "E501",
+              "F401",
+              "F403",
+              "F405",
+              "F841",
+              "I001",
+            },
+          },
+        },
+      },
+    })
+
+    require("mason").setup()
+
+    require("mason-lspconfig").setup({
+      ensure_installed = servers,
+      automatic_enable = {
+        exclude = { "pyright" },
+      },
+    })
+  end,
 }
