aca050330b7306328e9debe4d1f6b3e4c15a5f9b
Lemonish <lemonish22@pm.me>
2026-07-09 20:35:43 +0300

    nvim/img-clip: stop it from doing drag/drop pasting of images, only leader-p

--- nvim/lua/lemonish/lazy/img-clip.lua
+++ nvim/lua/lemonish/lazy/img-clip.lua
@@ -2,11 +2,15 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    default = {
+      drag_and_drop = {
+        enabled = false,
+      },
+      download_images = false,
+      verbose = false,
+    },
   },
   keys = {
-    -- suggested keymap
     { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
   },
 }
