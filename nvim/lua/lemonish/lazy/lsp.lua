4d602866c7c37bb6074717fc55a6bd9c33c1d0a4
Lemonish <lemonish22@pm.me>
2026-08-14 17:54:13 +0300

    nvim: typechecking off for python

--- nvim/lua/lemonish/lazy/lsp.lua
+++ nvim/lua/lemonish/lazy/lsp.lua
@@ -170,7 +170,7 @@ return {
       settings = {
         basedpyright = {
           analysis = {
-            typeCheckingMode = "basic",
+            typeCheckingMode = "off",
             diagnosticMode = "openFilesOnly",
             diagnosticSeverityOverrides = {
               reportWildcardImportFromLibrary = "none",
